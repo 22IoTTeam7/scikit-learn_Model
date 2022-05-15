e5c1d91c010e9b53a557a227a483213108d5f101
destiny3912 <destiny3910@gmail.com>
2022-05-15 17:35:02 +0900

    ADD : Models for each floor

---
+++ Floor4Model.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+Xtrain = pd.read_csv()
+Ytrain = pd.read_csv()
+
+rf_clf = RandomForestClassifier(random_state=0)
+rf_clf.fit(Xtrain, Ytrain)
+
+print(rf_clf.predict(x_test))
+
+'''4층 모델 파일'''
